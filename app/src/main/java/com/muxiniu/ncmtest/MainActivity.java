e84b1ca1eeea42d07d7940e575e41fa67d6ed16c
muxiniu <40936020+muxiniu@users.noreply.github.com>
2026-09-15 08:37:24 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/muxiniu/ncmtest/MainActivity.java
@@ -0,0 +1,10 @@
+package com.muxiniu.ncmtest;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle s) {
+        super.onCreate(s);
+        setContentView(android.R.layout.simple_list_item_1);
+    }
+}
